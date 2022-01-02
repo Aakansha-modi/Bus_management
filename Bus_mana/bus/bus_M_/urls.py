a143e1e44ea6cf90714c6afc6f478f8bbf52eb75
Aakansha-modi <56475108+Aakansha-modi@users.noreply.github.com>
2022-01-02 20:13:28 +0530

    Update urls.py

--- Bus_mana/bus/bus_M_/urls.py
+++ Bus_mana/bus/bus_M_/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('view_booking/', views.view_booking, name='view_booking'),
     path('admin_home/', views.admin_home, name='admin_home'),
-  
+    path('cancel_booking/<str:id>',views.cancel_booking,name='cancel_booking'),
 
     
 ]
